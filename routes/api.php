f1279b3e716754eef624c2ec674e6a9253bfb5b1
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2022-05-07 23:46:05 +0200

    Scopes

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::prefix('admin')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
 
-    Route::middleware(['auth:sanctum'])->group(function () {
+    Route::middleware(['auth:sanctum', 'scope.admin'])->group(function () {
         Route::get('user', [AuthController::class, 'user']);
         Route::post('logout', [AuthController::class, 'logout']);
     });
