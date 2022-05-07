af96b0720bc84d8b6fa6866e2f6359cfbe396a99
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2022-05-07 23:25:42 +0200

    Logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -44,4 +44,13 @@ class AuthController extends Controller
     {
         return $request->user();
     }
+
+    public function logout()
+    {
+        $cookie = \Cookie::forget('jwt');
+
+        return response([
+            'message' => 'success'
+        ])->withCookie($cookie); 
+    }
 }
