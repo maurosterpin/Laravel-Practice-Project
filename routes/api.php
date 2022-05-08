c4d510db0967f5336f1dbe958023d60b85519ab8
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2022-05-08 09:23:38 +0200

    Query Scopes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AmbassadorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -25,6 +26,8 @@ Route::prefix('admin')->group(function () {
         Route::post('logout', [AuthController::class, 'logout']);
         Route::put('users/info', [AuthController::class, 'updateInfo']);
         Route::put('users/password', [AuthController::class, 'updatePassword']);
+
+        Route::get('ambassadors', [AmbassadorController::class, 'index']);
     });
 });
 
